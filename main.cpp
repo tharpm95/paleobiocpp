fa2a21cd06af0c242ef07d22023b6742c80a563d
Matt Tharp <tharpmatt95@gmail.com>
2025-02-11 15:39:46 -0800

    Add data loading button

--- main.cpp
+++ main.cpp
@@ -1,13 +1,60 @@
 #include <QApplication>
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QFile>
+#include <QTextStream>
+
+class TaxonViewer : public QWidget {
+    Q_OBJECT
+
+public:
+    TaxonViewer(QWidget *parent = nullptr);
+
+private slots:
+    void loadData();
+
+private:
+    QTextEdit *dataDisplay;
+};
+
+TaxonViewer::TaxonViewer(QWidget *parent)
+    : QWidget(parent), dataDisplay(new QTextEdit(this)) {
+    
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    
+    QPushButton *loadButton = new QPushButton("Load Data", this);
+    layout->addWidget(loadButton);
+    layout->addWidget(dataDisplay);
+    
+    connect(loadButton, &QPushButton::clicked, this, &TaxonViewer::loadData);
+    
+    dataDisplay->setReadOnly(true);
+    setLayout(layout);
+    setWindowTitle("Taxon Data Viewer");
+    resize(600, 400);
+}
+
+void TaxonViewer::loadData() {
+    QFile file("taxon.tab");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        dataDisplay->setText("Failed to open the file.");
+        return;
+    }
+
+    QTextStream in(&file);
+    QString data = in.readAll();
+    dataDisplay->setText(data);
+}
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
-    QWidget window;
-    window.setWindowTitle("Simple Qt App");
-    window.resize(300, 200);
+    
+    TaxonViewer window;
     window.show();
-
+    
     return app.exec();
 }
+
+#include "main.moc"
